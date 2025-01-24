12549dbda2c30c1ab1a19706e66e6b89e5efad7a
nathanlu <nathanlumax@gmail.com>
2025-01-23 18:07:04 -0600

    initial commit (boilerplate and gitignore)

---
+++ Project1/yash/README.md
@@ -0,0 +1,10 @@
+# Instructions
+```bash
+cd Project1/yash
+rm -rf build
+mkdir build
+cd build
+cmake ..
+make
+./yash
+```
---
+++ Project1/yash/src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "job_control.h"
+#include "parser.h"
+#include "process.h"
+#include "signals.h"
+
+int main() {
+    printf("Shell initialized successfully.\n");
+
+    return 0;
+}
